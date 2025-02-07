64dfcbae5eb59e7ae0c6967282dc5ec0c48bd9d4
윤주연 <jyyun@didim365.co.kr>
2025-02-07 17:29:56 +0900

    modify: database

--- app/database.py
+++ app/database.py
@@ -12,41 +12,54 @@ pg_db = config.PG_DB
 
 DATABASE_URL = f"postgresql+psycopg2://{pg_username}:{pg_password}@localhost:{pg_port}/{pg_db}"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 metadata = MetaData()
 
 chat_info = Table("chat_info", metadata, autoload_with=engine)
 
-def insert_chat(user_id: str, chat_id: str, messages: list[dict]):
+# 데이터 삽입, 수정
+def upsert_chat(user_id: str, chat_id: str, messages: list[dict]):
     with engine.connect() as conn:
-        existing_user = conn.execute(chat_info.select().where(chat_info.c.user_id == user_id)).fetchone()
-        if existing_user:
-            print(f"Error: user_id '{user_id}' already exists!")
-            return
+        messages_str = json.dumps(messages)
 
-        # messages를 JSON 문자열로 변환
-        messages_str = json.dumps(messages)  # 리스트를 문자열로 변환
-        insert_query = chat_info.insert().values(
-            user_id=user_id,
-            chat_id=chat_id,
-            messages=messages_str
+        existing_chat = conn.execute(chat_info.select().where(chat_info.c.chat_id == chat_id)).fetchone()
+        if existing_chat:
+            update_query = (
+                chat_info.update()
+                .where(chat_info.c.chat_id == chat_id)
+                .values(messages=messages_str,
+                        created_at=func.now())
+            )
+            conn.execute(update_query)
+            conn.commit()
+            print(f"기존 chat_id '{chat_id}'의 messages가 업데이트되었습니다!")
+        else:
+            # 새로운 행 삽입
+            insert_query = chat_info.insert().values(
+                user_id=user_id,
+                chat_id=chat_id,
+                messages=messages_str
+            )
+            conn.execute(insert_query)
+            conn.commit()
+            print("새로운 데이터가 삽입되었습니다!")
+
+
+# 데이터 조회
+def get_chats(user_id: str, chat_id: str):
+    with engine.connect() as conn:
+        select_query = chat_info.select().where(
+            chat_info.c.user_id==user_id,
+            chat_info.c.chat_id==chat_id
         )
-        conn.execute(insert_query)
-        conn.commit()
-        print("데이터가 삽입되었습니다!")
-
-# 데이터 조회 함수
-def get_chats():
-    with engine.connect() as conn:
-        select_query = chat_info.select()
         result = conn.execute(select_query)
 
-        for row in result:
-            messages = json.loads(row.messages)
-            print(f"User ID: {row.user_id}, Chat ID: {row.chat_id}, Messages: {messages}, Created At: {row.created_at}")
+        for rows in result:
+            messages = json.loads(rows.messages)
+            print(f"User ID: {rows.user_id}, Chat ID: {rows.chat_id}, Messages: {messages}, Created At: {rows.created_at}")
 
 
 # 실행
 if __name__ == "__main__":
-    insert_chat("test_user_id", "test_chat_id2", [{"role": "user", "message": "안녕하세요!"}, {"role": "assistant", "message": "안녕하세요, 무엇을 도와드릴까요?"}])
-    get_chats()
+    upsert_chat("test_user_id2", "test_chat_id2", [{"role": "user", "message": "너눈 누구냐냐!"}, {"role": "assistant", "message": "안녕하세요, 무엇을 도와드릴까요?"}])
+    get_chats("test_user_id2", "test_chat_id2")
